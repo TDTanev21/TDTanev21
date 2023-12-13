8daa09f69b4877c2a6bc8aff89ec0de733a86658
Теодор Димитров Танев <TDTanev21@codingburgas.bg>
2023-12-13 14:33:15 +0200

    Add the data base

--- bussinessLogicTier.js
+++ bussinessLogicTier.js
@@ -2,10 +2,29 @@ function attemptLogin() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
+    // Read data from an external file (for educational purposes, not suitable for production)
+    var fileInput = document.createElement('input');
+    fileInput.type = 'file';
 
-    if (username === 'TDTanev21' && password === 'Tedo1234') {
-        alert('You\'ve successfully logged in!');
-    } else {
-        alert('Try again!');
-    }
+    fileInput.onchange = function() {
+        var file = fileInput.files[0];
+        var reader = new FileReader("dataTier.txt");
+
+        reader.onload = function(e) {
+            var data = e.target.result.split('\n');
+            var storedUsername = data[0].trim();
+            var storedPassword = data[1].trim();
+
+            // Simulate backend authentication
+            if (username === storedUsername && password === storedPassword) {
+                alert('You\'ve successfully logged in!');
+            } else {
+                alert('Try again!');
+            }
+        };
+
+        reader.readAsText(file);
+    };
+
+    fileInput.click();
 }
